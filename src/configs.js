3f9dbe7c988b6fbd5c4833b6e8d9a8e9a29b459b
Raven Jiang <raven@cs.stanford.edu>
2017-07-01 17:46:08 -0700

    Upgrades contract

--- src/configs.js
+++ src/configs.js
@@ -1,6 +1,6 @@
 // @flow
 
 const GRID_SIZE = 1000;
-const CONTRACT_ADDRESS = '0x0fcf52618819b74ba74b541d0bb82480b19ef1fa';
+const CONTRACT_ADDRESS = '0xf797f66b934b54e0ab3a24d2cdfd92f6c9469e4d';
 
 module.exports = { GRID_SIZE, CONTRACT_ADDRESS };
