74479a977343d48e03b9b9a004e2e996797d30fa
Raven Jiang <raven@cs.stanford.edu>
2017-07-03 09:41:30 -0700

    Update upload script to use Jimp and accept commandline arguments

--- src/scripts/uploader.js
+++ src/scripts/uploader.js
@@ -1,8 +1,9 @@
 // @flow
 
 const Grid = require('../ethereum/Grid.json');
-const getPixels = require('get-pixels');
+const Jimp = require('jimp');
 const Web3 = require('web3');
+const parseArgs = require('minimist');
 const web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
@@ -12,48 +13,58 @@ const { CONTRACT_ADDRESS, GRID_SIZE } = require('../configs');
 
 const contract = web3.eth.contract(Grid.abi).at(CONTRACT_ADDRESS);
 
-function drawImage(cornerX, cornerY, pixels) {
-  const [frames, width, height, dimensions] = pixels.shape.slice();
-
-  console.log(`Image size: ${width} x ${height}`);
-  // console.log(pixels.get(0, 300, 300, 1));
-
+function drawImage(image, cornerX, cornerY, skip) {
+  const { data, height, width } = image.bitmap;
   let count = 0;
-  for (let x = 118; x < width; x++) {
-    for (let y = 0; y < height; y++) {
-      const targetRow = cornerY + y;
-      const targetCol = cornerX + x;
-      if (targetCol >= GRID_SIZE || targetRow >= GRID_SIZE) {
-        continue;
-      }
-
-      const a = pixels.get(0, x, y, 3);
-      if (a === 0) {
-        continue;
-      }
-
-      const r = pixels.get(0, x, y, 0);
-      const g = pixels.get(0, x, y, 1);
-      const b = pixels.get(0, x, y, 2);
-      const color = colorConversion.rgbToDecimal(r, g, b);
-
-      count += 1;
-      const transaction = contract.setPixelColor(
-        targetRow, targetCol, color,
-        { from: web3.eth.accounts[0] },
-      );
-      // const transaction = 'test';
-
-      console.log(count, `rgb(${r}, ${g}, ${b}) at (x = ${x}, y = ${y}) - ${transaction}`);
+  image.scan(0, 0, width, height, function(x, y, idx) {
+    const targetRow = cornerY + y;
+    const targetCol = cornerX + x;
+    if (targetCol >= GRID_SIZE || targetRow >= GRID_SIZE) {
+      return;
     }
-  }
+
+    const a = data[idx + 3];
+    if (a === 0) {
+      return;
+    }
+
+    count += 1;
+    if (count <= skip) {
+      return;
+    }
+
+    const r = data[idx + 0];
+    const g = data[idx + 1];
+    const b = data[idx + 2];
+    const color = colorConversion.rgbToDecimal(r, g, b);
+
+    const transaction = contract.setPixelColor(
+      targetRow, targetCol, color, { from: web3.eth.coinbase },
+    );
+
+    console.log([
+      count,
+      `Txn ${transaction}`,
+      `rgb(${r}, ${g}, ${b}) at (${targetCol}, ${targetRow})`,
+    ].join(' - '));
+  });
 }
 
-getPixels('/Users/darkmirage/pikachu.gif', function(err, pixels) {
-  if (err) {
-    console.log('Bad image path');
-    return
-  }
-  drawImage(400, 300, pixels);
-  // const color = contract.getPixelColor(100, 100);
-});
+function run() {
+  const args = parseArgs(process.argv.slice(2), {
+    default: {
+      x: 460,
+      y: 480,
+      skip: 0,
+    },
+  });
+  Jimp.read(args._[0], function(err, image) {
+    if (err) {
+      console.log('Bad image path');
+      return
+    }
+    drawImage(image, args.x, args.y, args.skip);
+  });
+}
+
+run();
