844b6d2619915ee96bde8c0cb5477362f5441afd
Raven Jiang <raven@cs.stanford.edu>
2017-07-15 17:35:25 +0800

    Update address for Mainnet deployment

--- src/configs.js
+++ src/configs.js
@@ -1,9 +1,9 @@
 // @flow
 
-const CONTRACT_ADDRESS = '0x3AB7eb5F74a664b85af977D37549F86d0FF7D81f';
+const CONTRACT_ADDRESS = '0xae89c8d6b98432df28242899b3688cc8e3d45ea3';
 const DEFAULT_ZOOM = 4.0;
-const DEPLOYED_BLOCK = 412133;
-const ETHERSCAN_URL = 'https://rinkeby.etherscan.io';
+const DEPLOYED_BLOCK = 4024924;
+const ETHERSCAN_URL = 'https://etherscan.io';
 const GRID_SIZE = 1000;
 const MAX_ZOOM = 16;
 const MIN_ZOOM = 0.25;
